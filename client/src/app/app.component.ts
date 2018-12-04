a550285cea53e9e0b8898bfe7a5665e234d3cb13
dani <dani.rochaazevedo@student.vinci.be>
2018-12-04 16:36:54 +0100

    setup backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+	headers: new HttpHeaders({ 
+		'Access-Control-Allow-Origin':'*',
+	})
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'client';
+	title = 'client';
+	ajaxCall=function(){
+		this.http.get('http://localhost:3030/users/current').subscribe(data => {
+      console.log(data);
+    });
+	}
+	
+	constructor(private http:HttpClient) {}
+  
 }
