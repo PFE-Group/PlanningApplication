b2201f19cdb75d9dc1ee3d515b3bee1648d3750e
Thomas Ronsmans <thomas.ronsmans@gmail.com>
2018-12-05 11:50:32 +0100

    user-information first impl

--- client/src/app/components/schedule/components/user-information/user-information.component.ts
+++ client/src/app/components/schedule/components/user-information/user-information.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../../shared/models/user';
 
 @Component({
   selector: 'app-user-information',
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.css']
 })
+
 export class UserInformationComponent implements OnInit {
+  public user = new User();
+  logOutLogo : string;
 
   constructor() { }
 
   ngOnInit() {
+
+    user.firstName = "Ronsmans";
+    user.lastName = "Thomas";
+    user.login = "T.R";
+    user.profilePicture = "../../../../../assets/default_image_profile.png";
+    this.logOutLogo = "../../../../../assets/log_out_logo.png";
+
+  }
+
+  logOut(){
+    console.log("log out");
   }
 
 }
