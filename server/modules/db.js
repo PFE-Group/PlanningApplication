a9e41608a4eef94684f2912deafde82123af106d
auloin <ismaila.abdoulahi@student.vinci.be>
2018-12-07 11:35:02 +0100

    env variables config

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,6 +1,12 @@
 const firebase = require("firebase-admin");
 var serviceAccount = require('../firebase-access.json')
 
+serviceAccount.private_key = process.env.FIREBASE_PKEY.replace(/\\n/g, '\n');
+serviceAccount.client_email = process.env.FIREBASE_CLIENT_EMAIL;
+serviceAccount.client_id = process.env.FIREBASE_CLIENT_ID;
+serviceAccount.private_key_id = process.env.FIREBASE_KEY_ID;
+serviceAccount.project_id = process.env.FIREBASE_PROJECT_ID;
+
 //Initialize firebase
 let connect = () => {
     firebase.initializeApp({
