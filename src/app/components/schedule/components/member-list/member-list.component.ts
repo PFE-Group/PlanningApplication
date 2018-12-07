c695aafe81bb204b65067be04c8ec087346107b0
dani <dani.rochaazevedo@student.vinci.be>
2018-12-07 09:35:13 +0100

    new version with frontend

--- src/app/components/schedule/components/member-list/member-list.component.ts
+++ src/app/components/schedule/components/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
+  memberList: Array<User>;
+  roles: Array<string>;
+  @Input() user: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.memberList = Array<User>();
+    this.memberList.push(this.user);
+    this.roles = Array<string>();
+    this.roles.push('Admin');
+    this.roles.push('Membre');
+    this.roles.push('Invité');
   }
 
 }
