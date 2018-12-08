11fd7ba12c4446ac16ff339937c5b37a462eb719
auloin <ismaila.abdoulahi@student.vinci.be>
2018-12-08 16:09:05 +0100

    work on progress - add a user to a schedule

--- server/routes/plannings.js
+++ server/routes/plannings.js
@@ -30,10 +30,52 @@ router.get('/:id', function(req, res, next){
 })
 
 
-router.put("/:id/members", (req, res, next) => {
-    console.log(req.params)
-    console.log(req.body)
-    res.send("HELLO FROM SERVER")
+router.put("/:id/member", (req, res, next) => {
+    const login = req.body.login;
+    const role = req.body.role;
+    const id = req.params.id;
+    var planningDocRef = db.dbFirestore.collection("plannings").doc(id);
+    var userDocRef = db.dbFirestore.collection("users").where("login", "==", login);
+    db.dbFirestore.runTransaction( (transaction) => {
+        return transaction.get(planningDocRef).then( (planningDoc) => {
+            if(!planningDoc.exists) {
+                throw "No planning with id: [" + req.params.id + "]";
+            }
+            var {users, name} = planningDoc.data();
+            if(login in users) {
+                throw "User [" + login + "] already added";
+            }
+            
+            return transaction.get(userDocRef).then( (snap) => {
+                if(snap.empty) {
+                    throw "Unregistered user: ["+ login +"]";
+                }
+                snap.forEach( (doc) => {
+                    var {firstName, lastName, plannings} = doc.data();
+                    console.log(plannings)
+                    users[login] = {
+                        "lastName": lastName,
+                        "firstName": firstName,
+                        "role": role,
+                        "id": doc.id
+                    };
+                    
+                    plannings.push({
+                        "name": name,
+                        "id": id,
+                        "role": role
+                    });
+                    
+                    transaction.update(doc.ref, {plannings: plannings});
+                    transaction.update(planningDocRef, {users: users});
+                })
+            })
+        })
+    }).then( () => {
+        res.send("HELLO FROM SERVER")
+    }).catch( (error) => {
+        res.send("[Transaction failed]"+ error);
+    })
 });
 
 module.exports = router;
