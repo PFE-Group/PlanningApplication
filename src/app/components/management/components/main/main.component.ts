c6ac5fa93a02887a9ad31f08183908c444c2575a
dani <dani.rochaazevedo@student.vinci.be>
2018-12-12 13:35:19 +0100

    create taks / invite user ok

--- src/app/components/management/components/main/main.component.ts
+++ src/app/components/management/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -8,37 +8,41 @@ import {Component, OnInit} from '@angular/core';
 
 export class MainComponent implements OnInit {
   step = 0;
-  showError:boolean
-  showOk:boolean
-  messageError(){
-    this.showError=true;
+  showError: boolean
+  showOk: boolean
+  messageError() {
+    this.showError = true;
+    this.showOk = false;
+
   }
-  messageOK(){
-    this.showOk=true;
+  messageOK() {
+    this.showError = false;
+
+    this.showOk = true;
   }
   ngOnInit() {
-    this.showError=false;
-    this.showOk=false;
+    this.showError = false;
+    this.showOk = false;
 
   }
 
   setStep(index: number) {
-    this.showError=false;
-    this.showOk=false;
+    this.showError = false;
+    this.showOk = false;
 
     this.step = index;
   }
 
   nextStep() {
-    this.showError=false;
-    this.showOk=false;
+    this.showError = false;
+    this.showOk = false;
 
     this.step++;
   }
 
   prevStep() {
-    this.showError=false;
-    this.showOk=false;
+    this.showError = false;
+    this.showOk = false;
 
     this.step--;
   }
